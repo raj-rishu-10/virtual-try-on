3aa45c489058898e5a27a05905b657391cf8b805
RISHU RAJ <rishuraj89850@gmail.com>
2026-06-17 18:17:04 +0530

    Define all possible routes and add Coming Soon page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./components/Landing";
 import SoulSerumAR from "./components/foundationOverride";
-
+import ComingSoon from "./components/ComingSoon";
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-white">
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/lipsticks" element={<ComingSoon />} />
+          <Route path="/jewellery" element={<ComingSoon />} />
+          <Route path="/foundation-shades" element={<SoulSerumAR />} />
+          <Route path="/blush" element={<ComingSoon />} />
+          <Route path="/try-now" element={<ComingSoon />} />
+          {/* Support the old serum-try-on route too */}
           <Route path="/serum-try-on" element={<SoulSerumAR />} />
         </Routes>
       </div>
---
+++ src/components/ComingSoon.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
+
+const ComingSoon: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <div className="flex flex-col items-center justify-center min-h-[80vh] px-4 text-center">
+        <h1 className="text-5xl font-bold text-gray-800 mb-6">Coming Soon!</h1>
+        <p className="text-xl text-gray-600 mb-8">
+          We are currently working hard to bring this feature to you. Stay tuned!
+        </p>
+        <Link
+          to="/"
+          className="bg-custom-blue hover:bg-[#6B7AFF] text-white px-8 py-3 rounded-lg text-lg font-bold transition-colors"
+        >
+          Go Back Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
